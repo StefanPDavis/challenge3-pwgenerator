15de98010532fb7a5b15968bae51417b73898231
StefanPDavis <snoodle22@hotmail.com>
2023-12-20 17:53:43 -0800

    fixed some functions

--- Develop/script.js
+++ Develop/script.js
@@ -11,38 +11,67 @@ function generatePassword() {
   let selectedOptions = [];
   let typeSelection = false;
 
+  
+
   while (typeSelection == false) {
     let capital = getChoice("uppercase");
     let notCapital = getChoice("lowercase");
     let numericValue = getChoice("numbers");
     let specialCharacter = getChoice("symbols");
     if ((capital) || (notCapital) || (numericValue) || (specialCharacter)) {
-      charTypeSelected = true;
+      typeSelection = true;
     } else {
       window.alert("You must select at least one character type.")
     }
   }
-}
 
+  if (capital) {
+    selectedOptions = selectedOptions.concat(uppercase);
+  }
+  if (notCapital) {
+    selectedOptions = selectedOptions.concat(lowercase);
+  }
+  if (numericValue) {
+    selectedOptions = selectedOptions.concat(numbers);
+  }
+  if (specialCharacter) {
+    selectedOptions = selectedOptions.concat(symbols);
+  }
+
+  let passwordString = "";
+  for (let i = 0; i < passwordLength; i++) {
+    passwordString += selectedOptions[Math.floor(Math.random() * (selectedOptions.length))];
+  }
+
+return passwordString;
+}
 
 function getPasswordLength() {
   let userChoice = 0;
   while ((userChoice < 8) || (userChoice > 128)) {
     userChoice = parseInt(window.prompt("How long do you want your password to be? (between 8 and 128): "));
+    if (isNaN(userChoice)) {
+      userChoice = 0;
+    }
   }
+
+  return userChoice;
 }
 
-if (capital) {
-  selectedOptions = selectedOptions.concat(uppercase);
-}
-if (notCapital) {
-  selectedOptions = selectedOptions.concat(lowercase);
-}
-if (numericValue) {
-  selectedOptions = selectedOptions.concat(numbers);
-}
-if (specialCharacter) {
-  selectedOptions = selectedOptions.concat(symbols);
+function getChoice(currentOption) {
+  let userChoice = "a",
+    messagePrompt = "";
+  let messagePrompt = ('Do you want '.concat(currentOption));
+  messagePrompt = messagePrompt.concat(' characters? (y or n)');
+  while (userChoice = "a") {
+    userChoice = (window.prompt(messagePrompt));
+    userChoice = userChoice.toLowerCase();
+    if (userChoice == "y") {
+      return true;
+    } else if (userChoice == "n") {
+      return false;
+    }
+  }
 }
 
 // Get references to the #generate element
