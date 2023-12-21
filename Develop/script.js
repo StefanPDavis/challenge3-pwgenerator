37f734bfc691ccaf450bfe7eadf35afcc5594bec
StefanPDavis <snoodle22@hotmail.com>
2023-12-20 16:09:25 -0800

    added variables in javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,12 @@
 // Assignment code here
+let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", 
+"R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", 
+"r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", 
+"]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+let pwlength = [getPWLength];
 
 
 // Get references to the #generate element
@@ -14,4 +22,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", selectLength, selectUppercase, selectLowercase, selectNumbers, 
+selectSymbols, writePassword);
