33323c495d53ddc521fda6b42b6e6352054024db
StefanPDavis <snoodle22@hotmail.com>
2023-12-20 16:29:44 -0800

    renamed functions to be more readable in javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-function generatePW() {
+function generatePassword() {
   let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", 
   "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", 
@@ -7,10 +7,10 @@ function generatePW() {
   let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   let symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", 
   "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
-  let pwlength = [getPWLength];
+  let pwlength = [getPasswordLength];
 }
 
-function getPWLength() {
+function getPasswordLength() {
   let userChoice = 0;
   while ((userChoice < 8) || (userChoice > 128)) {
     userChoice = parseInt(window.prompt("How long do you want your password to be? (between 8 and 128): "));
